19c8fb4e0524ccdc4b63e93187375b7bff52cfdd
Atul Tathagat <atul.tathagat@lowes.com>
2024-02-26 17:30:34 +0530

    footer styling fixed

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <div>
+    <div className='footer'>
       <footer className="py-1 pt-5">
         <p className="text-center mt-1 fw-bold">
           ShopIT - 2024, All Rights Reserved
